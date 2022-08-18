14ce04a4e3a4907699c881bc95f2c70d8d2a5bb3
Diego Morales <mor21146@uvg.edu.gt>
2022-08-18 16:55:52 -0600

    Parte 2 y [arte 3 terminadas

---
+++ Parte 2/lab2parte2.cpp
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* FACULTAD DE INGENIERÍA
+* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
+*
+* Curso:       CC3086 - Programación de microprocesadores     Ciclo II - 2022
+* Descripción: Crear múltiples estructuras (arreglos de estructuras)
+------------------------------------------------------------------------------*/
+
+//
+#include <iostream>
+#include <bits/stdc++.h>
+//#include <stdlib>
+using namespace std;
+
+struct estudiante{
+    string nombre;
+    string apellido;
+    string cui;
+    int ultimo_cui;
+    string fecha_expiracion;
+}estudiante[3];
+
+int main(){
+    //for para crear estudiantes 
+    for(int i=0; i<3; i++){
+        fflush(stdin);
+        cout<<"Ingrese el nombre del estudiante: ";
+        cin>>estudiante[i].nombre;
+        cout<<"Ingrese el apellido del estudiante: ";
+        cin>>estudiante[i].apellido;
+        cout<<"Ingrese el CUI del estudiante: ";
+        cin>>estudiante[i].cui;
+        cout<<"Ingrese el ultimo numero del CUI del estudiante: ";
+        cin>>estudiante[i].ultimo_cui;
+        cout<<"Ingrese la fecha de expiración del CUI: ";
+        cin>>estudiante[i].fecha_expiracion;
+
+    }
+    //ordenar los cui por ultimo numero de cui
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(estudiante[i].ultimo_cui<estudiante[j].ultimo_cui){
+                swap(estudiante[i],estudiante[j]);
+            }
+        }
+    }
+
+    //mostrar los cui ordenados por ultimo numero de cui
+    cout<<"\nCUI ordenados: ";
+    for(int i=0; i<3; i++){
+        cout<<"\nCUI: "<<estudiante[i].cui;
+    }
+
+    
+    return 0;
+}
+
---
+++ Parte 4/archivo.cpp
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* FACULTAD DE INGENIERÍA
+* DEPARTAMENTO DE CIENCIA DE LA COMPUTACIÓN
+*Autor: Dieg Alejandro Morales Escobars
+* Curso:       CC3086 - Programación de microprocesadores     Ciclo II - 2022
+* Descripción: Crear múltiples estructuras (arreglos de estructuras)
+------------------------------------------------------------------------------*/
+
+//
+#include <iostream>
+#include <stdlib.h>
+#include <pthread.h>
+#define NTHREADS 2
+using namespace std;
+//create dynamic array
+int size, half;
+int *array=new int[size];
+
+int* createArray(int size){
+    int* array = new int[size];
+    return array;
+}
+//fill array with random numbers
+void fillArray(int* array, int size){
+    for(int i=0; i<size; i++){
+        array[i] = rand()%100;
+    }
+}
+//pthread function to add numbers in the fisrt half of the array
+void* addArray(void* tnumber){
+    long tID = (long long)tnumber;
+    int sum = 0;
+    if(tID == 0){
+        for(int i=0; i<half; i++){
+            sum += array[i];
+        }
+    }
+    else{
+        for(int i=half; i<size; i++){
+            sum += array[i];
+        }
+    }
+    cout << "\nSuma de la mitad "<< tID+1 <<" del array: " << sum << endl;
+    pthread_exit(NULL);
+}
+
+//main
+int main(){
+    //ask user for array size
+    cout<<"Ingrese el tamaño del arreglo: ";
+    size;
+    cin>>size;
+    //create array
+    array = createArray(size);
+    //fill array with random numbers
+    fillArray(array, size);
+    //print array
+    for(int i=0; i<size; i++){
+        cout<<array[i]<<" ";
+    }
+    
+    int rc; 
+    pthread_t tID;
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); //set thread to joinable
+    half = size/2;
+    for(long i=0; i<NTHREADS;i++){ //create threads
+        rc = pthread_create(&tID, &attr, addArray, (void*)i);
+        if(rc){ //if error
+            cout<<"Error al crear el hilo"<<endl;
+            exit(-1);
+        }
+
+        rc=pthread_join(tID, NULL); //wait for thread to finish
+        if(rc){ //if error
+            cout<<"Error al unir el hilo"<<endl;
+            exit(-1);
+        }
+    }
+
+    pthread_attr_destroy(&attr); //destroy thread attributes
+    pthread_exit(NULL); //exit thread
+    return 0;
+}
+
+
